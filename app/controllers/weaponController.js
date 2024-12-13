c975f88297651e6f48740f9f596a107500f576ce
Olga Morozova <olga.ovsejchik@gmail.com>
2024-12-13 14:02:47 +0100

    Controller and routes for the Weapon model implemented

---
+++ app/controllers/weaponController.js
@@ -0,0 +1,69 @@
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+const { Weapon } = require('../models/index')(Sequelize, config);
+
+const createItem = ('/api/weapons', async (req, res) => {
+    try {
+        const weapon = await Weapon.create(req.body);
+        res.status(201).json(weapon);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+const getAll = ('/', async (req, res) => {
+    try {
+        const { count, rows }  = await Weapon.findAndCountAll({});
+
+        if (!count) {
+            return res.status(200).json({ message: 'No weapons found.'});
+        }
+
+        res.status(200).json(rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const getSingeItem = ('/:id', async (req, res) => {
+    try {
+        const weapon = await Weapon.findByPk(req.params.id);
+
+        if (!weapon) return res.status(404).json({ error: 'Weapon not found.' });
+        
+        res.status(200).json(weapon);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const updateItem = ('/:id', async (req, res) => {
+    try {
+        const weapon = await Weapon.findByPk(req.params.id);
+
+        if (!weapon) return res.status(404).json({ error: 'Weapon not found.' });
+        
+        await weapon.update(req.body);
+        
+        await weapon.save();
+        res.status(200).json(weapon);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+const deleteItem = ('/:id', async (req, res) => {
+    try {
+        const weapon = await Weapon.findByPk(req.params.id);
+
+        if (!weapon) return res.status(404).json({ error: 'Weapon not found.' });
+        
+        await weapon.destroy();
+        
+        res.status(204).send('Weapon deleted.');
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = { getAll, getSingeItem, createItem, updateItem, deleteItem };
---
+++ app/routes/weaponRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { getAll, getSingeItem, createItem, updateItem, deleteItem } = require('../controllers/weaponController');
+
+const router = express.Router();
+
+router.get('/', getAll);
+router.get('/:id', getSingeItem);
+router.post('/', createItem);
+router.patch('/:id', updateItem);
+router.delete('/:id', deleteItem);
+
+module.exports = router;
