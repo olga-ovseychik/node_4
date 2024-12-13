5e189d8567db4ade4a8cfa105dc3c79283f527df
Olga Morozova <olga.ovsejchik@gmail.com>
2024-12-13 13:47:20 +0100

    Associations implemented

--- app/models/index.js
+++ app/models/index.js
@@ -18,6 +18,13 @@ module.exports = (Sequelize, config) => {
 	const Weapon = WeaponModel(Sequelize, sequelize);
 	const Pizza = PizzaModel(Sequelize, sequelize);
 
+    Turtle.belongsTo(Weapon, { foreignKey: 'weaponId' });
+	Turtle.belongsTo(Pizza, { foreignKey: 'firstFavoritePizzaId', as: 'firstFavoritePizza' });
+	Turtle.belongsTo(Pizza, { foreignKey: 'secondFavoritePizzaId', as: 'secondFavoritePizza' });
+
+	Pizza.hasMany(Turtle, { foreignKey: 'firstFavoritePizzaId', as: 'firstFavoriteOfTurtles' });
+	Pizza.hasMany(Turtle, { foreignKey: 'secondFavoritePizzaId', as: 'secondFavoriteOfTurtles' });
+
 	return {
 		Turtle,
 		Weapon,
